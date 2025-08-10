a23976bd75b03455d00fcdb12b1f7f236465963b
Avhishek Nandi <avhisheknandi03@gmail.com>
2025-08-11 01:58:04 +0530

    frontend added

--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,11 +1,18 @@
-import express, { Router } from 'express'
-import isAuth from '../middlewares/isAuth.js';
-import { createNewChat } from '../controllers/chat.js';
+import express from "express";
+import isAuth from "../middlewares/isAuth.js";
+import {
+  createNewChat,
+  getAllChats,
+  getMessagesByChat,
+  sendMessage,
+} from "../controllers/chat.js";
+import { upload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
+router.post("/chat/new", isAuth, createNewChat);
+router.get("/chat/all", isAuth, getAllChats);
+router.post("/message", isAuth, upload.single("image"), sendMessage);
+router.get("/message/:chatId", isAuth, getMessagesByChat);
 
-
-router.post("/chat/new",isAuth,createNewChat);
-
-export default Router
+export default router;
